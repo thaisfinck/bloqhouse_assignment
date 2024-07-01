8d3db86ad51b3350e66bc811d7043f5da1525cf9
thaisfinck <thaisfinck@gmail.com>
2024-07-01 22:40:24 +0200

    create the edit movies page

---
+++ src/components/Movies/Edit.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container" style="margin-top: 1rem">
+    <h3>Edit Movie</h3>
+    <form>
+      <div class="form-group">
+        <label for="title">Title</label>
+        <input type="text" id="title" class="form-control" />
+      </div>
+      <div class="form-group">
+        <label>Genre</label>
+        <select class="form-select">
+          <option value="action">Action</option>
+          <option value="comedy">Comedy</option>
+          <option value="drama">Drama</option>
+          <option value="horror">Horror</option>
+          <option value="sci-fi">Sci-Fi</option>
+        </select>
+      </div>
+      <div class="form-group">
+        <label for="year">Year</label>
+        <input type="number" id="year" class="form-control" />
+      </div>
+      <div class="form-group">
+        <label for="poster">Poster</label>
+        <input type="text" id="poster" class="form-control" />
+      </div>
+      <button type="submit" class="btn btn-primary" style="margin-top: 1rem">
+        Update Movie
+      </button>
+    </form>
+  </div>
+</template>
