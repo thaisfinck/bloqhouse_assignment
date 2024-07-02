9f138a4e2b6eaf4c69c8a0080316bb3914cdae7b
thaisfinck <thaisfinck@gmail.com>
2024-07-02 18:35:26 +0200

    add the add movie request

--- src/components/Movies/Add.vue
+++ src/components/Movies/Add.vue
@@ -1,14 +1,45 @@
+<script setup>
+import { addDoc, collection } from "firebase/firestore";
+import { db, refreshMovies } from "../../fireBaseConfig";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const addMovie = () => {
+  const title = document.getElementById("title").value;
+  const genre = document.getElementById("genre").value;
+  const year = document.getElementById("year").value || null;
+  const poster = document.getElementById("poster").files[0] || null;
+
+  const newMovie = {
+    title,
+    genre,
+    year,
+    poster,
+  };
+
+  addDoc(collection(db, "movies"), newMovie);
+  refreshMovies();
+};
+
+const submitForm = (e) => {
+  e.preventDefault();
+  addMovie();
+  router.push("/");
+};
+</script>
+
 <template>
   <div class="container" style="margin-top: 1rem">
     <h3>Add Movie</h3>
     <form>
       <div class="form-group">
-        <label for="title">Title</label>
-        <input type="text" id="title" class="form-control" />
+        <label for="title" lass="form-label">Title</label>
+        <input type="text" id="title" class="form-control" required />
       </div>
       <div class="form-group">
-        <label>Genre</label>
-        <select class="form-select">
+        <label for="genre" class="form-label">Genre</label>
+        <select class="form-select" id="genre" required>
           <option value="action">Action</option>
           <option value="comedy">Comedy</option>
           <option value="drama">Drama</option>
@@ -17,16 +48,25 @@
         </select>
       </div>
       <div class="form-group">
-        <label for="year">Year</label>
+        <label for="year" lass="form-label">Year</label>
         <input type="number" id="year" class="form-control" />
       </div>
-      <div class="form-group">
-        <label for="poster">Poster</label>
-        <input type="text" id="poster" class="form-control" />
+      <div class="mb-3">
+        <label for="poster" class="form-label">Upload Poster</label>
+        <input type="file" class="form-control" id="poster" />
       </div>
-      <button type="submit" class="btn btn-primary" style="margin-top: 1rem">
-        Add Movie
-      </button>
+      <div class="mb-3" style="display: flex; gap: 1rem; margin-top: 1rem">
+        <button type="submit" class="btn btn-primary" @click="submitForm">
+          Add Movie
+        </button>
+        <button
+          type="button"
+          class="btn btn-secondary"
+          @click="router.push('/')"
+        >
+          Cancel
+        </button>
+      </div>
     </form>
   </div>
 </template>
