18547754f5459b20f1a8b098cf076f9e0c219c6e
thaisfinck <thaisfinck@gmail.com>
2024-07-04 12:43:29 +0200

    Mock uploading file and fix the show page

--- src/components/Movies/Show.vue
+++ src/components/Movies/Show.vue
@@ -26,25 +26,23 @@ export default {
 <template>
   <div class="container" style="margin-top: 1rem" v-if="movie">
     <h3>{{ movie.title }}</h3>
-    <div class="row">
-      <div class="col-md-4" v-if="movie.poster">
-        <img
-          :src="movie.poster"
-          alt="movie poster"
-          style="width: 100%; height: auto"
-        />
-      </div>
-      <div class="col-md-8">
-        <p><strong>Genre:</strong> {{ movie.genre }}</p>
-        <p><strong>Year:</strong> {{ movie.year }}</p>
-      </div>
+    <div class="col-md-8" v-if="movie.poster">
+      <img
+        :src="movie.poster"
+        alt="movie poster"
+        style="width: 100%; height: auto"
+      />
+    </div>
+    <div class="col-md-8">
+      <p><strong>Genre:</strong> {{ movie.genre }}</p>
+      <p><strong>Year:</strong> {{ movie.year }}</p>
+    </div>
 
-      <div class="col-12" style="margin-top: 1rem; display: flex; gap: 1rem">
-        <RouterLink :to="'/edit/' + movie.id" class="btn btn-primary">
-          <i class="fa-solid fa-edit"></i>
-          Edit
-        </RouterLink>
-      </div>
+    <div class="col-12" style="margin-top: 1rem; display: flex; gap: 1rem">
+      <RouterLink :to="'/edit/' + movie.id" class="btn btn-primary">
+        <i class="fa-solid fa-edit"></i>
+        Edit
+      </RouterLink>
     </div>
   </div>
 </template>
